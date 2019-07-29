73354ee26084fc8c4488396a4e2730d79752d21d
han-yan-ds <hanprogramming@gmail.com>
2019-07-29 14:40:07 -0600

    Done all but last

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// TODO
+// var GroceryList = function(props) {
+//   return (
+//     <ul>
+//       {props.groceryItems.map( function(item) {
+//         return <li>{item}</li>;
+//       } )}
+//     </ul>
+//   );
+// };
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <li onMouseOver={this.makeBold}>{this.props.item}</li>;
+  }
+
+  makeBold() {
+    // $(".groceryItem").css('font-weight', 'bold');
+  }
+};
+
+var GroceryList = function(props) {
+  return (
+    <ul>
+      {props.groceryItems.map( function(item){
+        return <GroceryListItem key={i} item={item} />;
+      })}
+    </ul>
+  );
+};
+    
+ReactDOM.render(<GroceryList groceryItems={["cucumbers", 'kale', 'bananas']} />, document.getElementById("app"));
